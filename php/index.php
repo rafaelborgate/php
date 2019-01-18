4d385e9c5d0ca2d27644421a1edaab9854c5a9cd
Rafael Borgate <borgate@gmail.com>
2019-01-18 11:50:07 -0200

    Arrays

--- php/index.php
+++ php/index.php
@@ -1,5 +1,30 @@
 <?php
   $nome = "Aprendizado PHP";
   echo $nome;
-  echo "modificação";
+  var_dump($nome); //exibe detalhes da variável
+
+  $umbooleano = TRUE;
+  $umaString = "PHP";
+  $uminteiro = 123;
+
+  echo gettype($umaString)."<br>";
+  echo gettype($umbooleano)."<br>";
+  echo gettype($uminteiro)."<br>";
+
+  /*a função is_tipo retorna true ou false para a verificação do is_tipo
+  de variável
+  is_int , is_string  etc
+  */
+  if(is_int($uminteiro)){
+    echo "Esta variável é um inteiro <br>";
+  }
+
+  /*ARRAYS*/
+  //Arrays indexados sem chaves
+  $vetor = array("branco","vermelho", "preto");
+  var_dump($vetor);
+
+  //Arrays indexados sem chaves
+  $vetorCores = array("cor1"=>"branco", "cor2"=>"vermelho","cor3"=>"preto");
+  var_dump($vetorCores);  
  ?>
